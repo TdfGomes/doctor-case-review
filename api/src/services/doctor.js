65c2e51cbbbefab4b0f9d1a6f9d765ff69c90ea4
Tiago Gomes <t.dfgomes@icloud.com>
2020-08-12 17:46:30 +0100

    refactor: get current doctor from auth middleware

--- api/src/services/doctor.js
+++ api/src/services/doctor.js
@@ -1,19 +1,13 @@
 const { Doctor } = require('../models')
-const { decode } = require('./auth')
 
 module.exports = {
   getDoctorByUserName: async (username) => {
     try {
       const doctor = await Doctor.findOne({ username }).select('+password')
+      if (!doctor) {
+        throw new Error('There are no users')
+      }
       return doctor
-    } catch (error) {
-      throw new Error(error)
-    }
-  },
-  getCurrentDoctor: async (token) => {
-    try {
-      const currentDoctor = await decode(token, process.env.JWT_SECRET)
-      return currentDoctor
     } catch (error) {
       throw new Error(error)
     }
