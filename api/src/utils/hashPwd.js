1c4759e98c3739060edf908109c32351e5ef199e
Tiago Gomes <t.dfgomes@icloud.com>
2020-08-13 20:05:08 +0100

    reactor: prettify

--- api/src/utils/hashPwd.js
+++ api/src/utils/hashPwd.js
@@ -1,24 +1,24 @@
-const crypto = require("crypto");
+const crypto = require('crypto')
 
 module.exports = {
   hash: async (password) => {
     return new Promise((resolve, reject) => {
-      const salt = crypto.randomBytes(8).toString("hex");
+      const salt = crypto.randomBytes(8).toString('hex')
 
-      crypto.scrypt(password, salt, 64, (err, derivedKey) => {
-        if (err) reject(err);
-        resolve(`${salt}:${derivedKey.toString("hex")}`);
-      });
-    });
+      crypto.scrypt(password, salt, 64, (error, derivedKey) => {
+        if (error) reject(error)
+        resolve(`${salt}:${derivedKey.toString('hex')}`)
+      })
+    })
   },
 
   verify: async (password, hash) => {
     return new Promise((resolve, reject) => {
-      const [salt, key] = hash.split(":");
-      crypto.scrypt(password, salt, 64, (err, derivedKey) => {
-        if (err) reject(err);
-        resolve(key == derivedKey.toString("hex"));
-      });
-    });
+      const [salt, key] = hash.split(':')
+      crypto.scrypt(password, salt, 64, (error, derivedKey) => {
+        if (error) reject(error)
+        resolve(key == derivedKey.toString('hex'))
+      })
+    })
   },
-};
+}
